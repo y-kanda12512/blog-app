6b391a337020039d3142297925eb37b76503e7db
yuto <y.kanda839yuzyou@gmail.com>
2024-09-19 22:43:00 +0900

    ビルドエラー修正後

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 // This file is automatically compiled by Webpack, along with any other files
+
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
@@ -15,3 +16,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// app/javascript/packs/application.js
+console.log('Webpacker is working!');
