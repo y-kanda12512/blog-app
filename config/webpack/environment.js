932f159dc3d5687d52db7d312be543aa3de7beb3
yuto <y.kanda839yuzyou@gmail.com>
2024-10-02 15:10:59 +0900

    ローカルwebpackビルド成功

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require("@rails/webpacker");
 
+// 不正なプロパティを削除して、サポートされているプロパティのみに設定
+environment.node = {
+  __dirname: true, // 必要に応じて true に設定
+  __filename: true, // 必要に応じて true に設定
+  global: true, // 必要に応じて true に設定
+};
+
 module.exports = environment;
