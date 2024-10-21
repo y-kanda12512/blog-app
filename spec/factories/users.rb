9dc91116a459311219f1d054845e6c984030557a
yuto <y.kanda839yuzyou@gmail.com>
2024-10-21 16:21:52 +0900

    systemスペック

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
-    factory :user do
-      email { Faker::Internet.email }
-      password { 'password' }
+  factory :user do
+    email { Faker::Internet.email }
+    password { 'password' }
+
+    # ユーザーがビルドされた時点でプロフィールもビルドする
+    trait :with_profile do
+      after :build do |user|
+        build(:profile, user: user)
+      end
     end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,4 +68,5 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
